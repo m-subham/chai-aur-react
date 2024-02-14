7f4d94db746f33f48802ae1ae8a8ef2627b0974c
Subham Maharana <121706810+m-subham@users.noreply.github.com>
2024-02-14 19:57:23 +0530

    first changes

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <h1>Hello world - chai aur react
+    Subham Maharana learning from class 01
+    1.easy to undestand the terminal 
+      2.simple explain the unnecessary code and remove it.
+      3.Thank you
+      </h1>
+  );
+}
+
+export default App;
